01987e5cde8ead7d876f1abb80e4c7b130cbcdb5
tamyk <kolawoletamilore1@gmail.com>
2023-10-20 21:17:10 +0100

    feat: created contact controller

--- controller/contact_controller.js
+++ controller/contact_controller.js
@@ -1,1 +1,5 @@
-module.exports 
+exports.createContact=(req,res)=>{}
+exports.allContact=(req,res)=>{}
+exports.singleContact=(req,res)=>{}
+exports.updateContact=(req,res)=>{}
+exports.deleteContact=(req,res)=>{}
