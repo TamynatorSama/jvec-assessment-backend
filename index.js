e45ce8e4ba44adca8299e609863c93e75936d2f8
tamyk <kolawoletamilore1@gmail.com>
2023-10-21 20:12:32 +0100

    chore: express setup

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-const { contentType } = require('express/lib/response');
-const http = require('http');
-const ResponseClass = require('./helper_class/http_response')
-const getReq = require('./http_methods/get_request')
-const postReq = require('./http_methods/post_request')
-const putReq = require('./http_methods/put_request')
-const deleteReq = require('./http_methods/delete_request')
-
 require('dotenv').config();
+const express = require('express')
+const app = express();
+const appRouter = require('./router/index')
 
 const port  = process.env.PORT || 5001;
 
-const server = http.createServer((req,res)=>{
+// middleware For parsing application/json
+app.use(express.json());
 
-    const response = new ResponseClass({res})
 
-    switch(req.method){
-        case "GET":
-            getReq(req,response);
-            break;
-        case "Post":
-            postReq(req,response);
-            break;
-        case "PUT":
-            putReq(req,response);
-            break;
-        case "DELETE":
-            deleteReq(req,response);
-            break;
-        default :
-        response.send({})
-    }
-
+app.use('/api',appRouter);
+ 
+app.listen(port, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", port);
 });
 
-server.listen(port,()=>{
-    console.log(`server start on port: ${port}`);
-});
+
+
+
+// const http = require('http');
+// const ResponseClass = require('./helper_class/http_response')
+// const getReq = require('./http_methods/get_request')
+// const postReq = require('./http_methods/post_request')
+// const putReq = require('./http_methods/put_request')
+// const deleteReq = require('./http_methods/delete_request')
+
+// require('dotenv').config();
+
+// const port  = process.env.PORT || 5001;
+
+// const server = http.createServer((req,res)=>{
+
+//     const response = new ResponseClass({res})
+
+//     switch(req.method){
+//         case "GET":
+//             getReq(req,response);
+//             break;
+//         case "POST":
+//             postReq(req,response);
+//             break;
+//         case "PUT":
+//             putReq(req,response);
+//             break;
+//         case "DELETE":
+//             deleteReq(req,response);
+//             break;
+//         default :
+//         response.send({
+//             statusCode: 400,
+//             body:{
+//                 "messgae":"invalid method on route"
+//             }
+//         })
+//     }
+
+// });
+
+// server.listen(port,()=>{
+//     console.log(`server start on port: ${port}`);
+// });
