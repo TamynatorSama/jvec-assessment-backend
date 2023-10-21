79a68be5a2e2a5677da771d23503cd81d59cf4e6
tamyk <kolawoletamilore1@gmail.com>
2023-10-21 21:04:17 +0100

    feat: added user and contact schema

---
+++ model/contact_model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const contactSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    profile_picture: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    facebook: {
+        type: String,
+    },
+    twitter: {
+        type: String,
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.Model("Contact",contactSchema)
