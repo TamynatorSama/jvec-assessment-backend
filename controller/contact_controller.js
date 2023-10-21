ae2b6f6c30157495def3a4e83eb6f783f415243a
tamyk <kolawoletamilore1@gmail.com>
2023-10-21 23:57:30 +0100

    feat: added update and delete functionalities

--- controller/contact_controller.js
+++ controller/contact_controller.js
@@ -67,5 +67,88 @@ exports.singleContact = async(req, res) => {
         result: contactList
     }))
 }
-exports.updateContact = (req, res) => { }
-exports.deleteContact = (req, res) => { }
+exports.updateContact = async (req, res) => {
+    let contactId = req.params.id;
+
+    //checking the db for corresponding contact
+    let contact = await contactModel.findOne({ "user_id": req.user_id ,"_id":contactId})
+    if(!contact)
+        return res.status(404).send(new ErrorResponse({
+            message: "Resource not found",
+            statusCode: 404,
+        }))
+
+
+    // getting needed fields form the request body
+    const { first_name, last_name, phone_number, twitter, facebook, email } = req.body
+
+    //VALIDATING NECESSARY FIELDS
+    // if (!first_name || !last_name, !phone_number) {
+
+    //     let firstNameVal = !first_name ? [
+    //         "First Name field is required"
+    //     ] : undefined
+    //     let lastNameVal = !last_name ? [
+    //         "Last Name field is required"
+    //     ] : undefined
+    //     let phoneVal = !phone_number ? [
+    //         "Last Name field is required"
+    //     ] : undefined
+
+    //     return res.status(400).json(
+    //         new ErrorResponse({
+    //             message: "Validation Error",
+    //             statusCode: 400,
+    //             error: {
+    //                 first_name: firstNameVal,
+    //                 last_name: lastNameVal,
+    //                 phone_number: phoneVal
+    //             }
+    //         })
+    //     )
+    // }
+
+    //updating the contact info
+
+    try{
+        await contact.updateOne(
+            {
+                first_name,
+                last_name,
+                phone_number,
+                email,
+                facebook,
+                twitter
+            }
+        ).exec()
+        res.status(200).send(new SuccessResponse({
+            message: "Contact updated Successfully",
+            statusCode: 200,
+        }))
+    }
+    catch(e){
+        return res.status(500).json(
+            new ErrorResponse({
+                message: "An error occurred",
+                statusCode: 500,
+                error: e
+            })
+        )
+    }
+ }
+
+
+exports.deleteContact = async (req, res) => { 
+    let contactId = req.params.id;
+    let contactList = await contactModel.findOneAndDelete({ "user_id": req.user_id ,"_id":contactId})
+    if(!contactList)
+        return res.status(404).send(new ErrorResponse({
+            message: "Resource not found",
+            statusCode: 404,
+        }))
+    return res.status(200).send(new SuccessResponse({
+        message: "Resource has been deleted Successfully",
+        statusCode: 200,
+    }))
+
+}
