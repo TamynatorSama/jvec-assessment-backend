fea4dd7e6c42f28aa7fcf1cfe2df763bcf3f04f1
tamyk <kolawoletamilore1@gmail.com>
2023-10-21 21:05:19 +0100

    chore: code refactor

--- router/index.js
+++ router/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const contactRouter = require('./contact_router')
+const {ErrorResponse} = require('../helper_class/json_response_class')
 
 
 router.use('/contact',contactRouter)
-// router.all('',(req,res)=>{
-//     res.json({
-//         "message": "route not found"
-//     })
-// })
+router.all('/*',(req,res)=>{
+    res.status(404).json(new ErrorResponse({message:"Route not found",statusCode:400}).toJson())
+})
 
 
 module.exports = router
